fd25e0c15d134557bfc5c2c5f1ca7abf5fc1fb5e
Scu11 <scu11@strongholdrsps.com>
2024-04-02 21:57:48 +0100

    Reorder fields in WindowMode

--- runescape/src/main/java/com/jagex/core/constants/WindowMode.java
+++ runescape/src/main/java/com/jagex/core/constants/WindowMode.java
@@ -1,9 +1,12 @@
 package com.jagex.core.constants;
 
 public final class WindowMode {
-    public static final int FULLSCREEN = 3;
-    public static final int RESIZABLE = 2;
+
     public static final int FIXED = 1;
+
+    public static final int RESIZABLE = 2;
+
+    public static final int FULLSCREEN = 3;
 
     private WindowMode() {
         /* empty */

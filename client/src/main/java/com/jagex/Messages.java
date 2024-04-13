ba27e97fbbd204fdb2324917293e1a6d3022be44
Scu11 <scu11@strongholdrsps.com>
2024-04-13 02:06:11 +0100

    Add file protocol scheme to Messages

--- client/src/main/java/com/jagex/Messages.java
+++ client/src/main/java/com/jagex/Messages.java
@@ -4,11 +4,11 @@ public final class Messages {
 
     public static final String FIRST_PATH_MISSING = "Provide path to public RSA key as first program argument";
 
-    public static final String PUBLIC_KEY_NOT_FOUND = "No public key file found at: %s";
+    public static final String PUBLIC_KEY_NOT_FOUND = "No public key file found at: file://%s";
 
-    public static final String JS5_KEY_READING = "JS5 public key reading from: %s";
+    public static final String JS5_KEY_READING = "JS5 public key reading from: file://%s";
 
-    public static final String LOGIN_KEY_READING = "Login public key reading from: %s";
+    public static final String LOGIN_KEY_READING = "Login public key reading from: file://%s";
 
     public static final String LOGIN_KEY_REUSING_JS5 = "Login public key reusing JS5 public key";
 
